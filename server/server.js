c5f6e52da1493b44cfbb3e664099f549e2a33237
John Clampet <jsclampet@gmail.com>
2023-12-12 07:35:31 -0600

    more refining data from tweet GET

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const baseURL = "https://api.twitter.com/2";
 const token =
   "AAAAAAAAAAAAAAAAAAAAAPly9QAAAAAAQP4Qf6PfN0NeU4L5keo%2B7kae%2Fs0%3DEQIp2W7jkVldFBLvOOFtSJXl2vWEe3f1J1STKMTyWEbsogNYfE";
 const getTweetsUrl =
-  "https://api.twitter.com/2/tweets/search/recent?tweet.fields=public_metrics&expansions=author_id&query=";
+  "https://api.twitter.com/2/tweets/search/recent?user.fields=profile_image_url&tweet.fields=text,public_metrics&expansions=author_id&query=";
 
 const apiClient = axios.create({
   baseURL,
@@ -29,16 +29,29 @@ app.get("/api/users/:username", (req, res) => {
 
 app.get("/api/tweet/:query", (req, res) => {
   const responseObject = {};
+  const responseObjectArray = [];
   apiClient.get(getTweetsUrl + req.params.query).then(({ data }) => {
-    responseObject.retweetCount = JSON.stringify(
-      data.data[0].public_metrics.retweet_count
-    );
-    responseObject.like_Count = JSON.stringify(
-      data.data[0].public_metrics.like_count
-    );
-    responseObject.username = JSON.stringify(data.includes.users[0].username);
-    console.log(responseObject);
-    res.send(JSON.stringify(data.data[0].public_metrics.retweet_count));
+    data.data.forEach((dataItem, index) => {
+      responseObject.retweet_count = JSON.stringify(
+        dataItem.public_metrics.retweet_count
+      );
+      responseObject.like_Count = JSON.stringify(
+        dataItem.public_metrics.like_count
+      );
+      responseObject.username = JSON.stringify(
+        data.includes.users[index].username
+      );
+      responseObject.profile_image_url = JSON.stringify(
+        data.includes.users[index].profile_image_url
+      );
+      responseObject.like_Count = JSON.stringify(
+        dataItem.public_metrics.like_count
+      );
+      responseObject.tweet_text = JSON.stringify(dataItem.text);
+      responseObjectArray.push(responseObject);
+    });
+    console.log(responseObjectArray);
+    res.send(responseObjectArray);
   });
 });
 
