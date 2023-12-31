2f63154b8ee439c4aaf71fafa43d1b5fbcaefa07
John Clampet <jsclampet@gmail.com>
2023-12-31 16:04:13 -0700

    started adding functionality

--- front-end/src/pages/Search/Search.tsx
+++ front-end/src/pages/Search/Search.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 const Search = () => {
-  const [apiOption, setApiOption] = useState("");
-  const handleSubmit = (apiUrl) => {};
+  const [apiOption, setApiOption] = useState("tweet");
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleSubmit = () => {
+    apiOption === "tweet"
+      ? axios.post(`http://localhost:3002/api/tweets/`, searchQuery)
+      : axios.post(`http://localhost:3002/api/users/`, searchQuery);
+  };
 
   return (
     <form onSubmit={handleSubmit} className="search-container">
@@ -16,7 +22,11 @@ const Search = () => {
           <option value="tweet">TWEET</option>
           <option value="user">USER</option>
         </select>
-        <input type="text" placeholder="Search by user or tweet content" />
+        <input
+          onInput={(e) => setSearchQuery(e.currentTarget.value)}
+          type="text"
+          placeholder="Search by user or tweet content"
+        />
         <button type="submit">Search</button>
       </div>
     </form>
